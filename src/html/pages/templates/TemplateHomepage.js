377d9a62aa89087fc892a7d42074d1fc87b50bd1
tommy <tommy@syndicut.com>
2022-08-06 20:01:10 +0100

    Updated requests
    Updated requests

--- src/html/pages/templates/TemplateHomepage.js
+++ src/html/pages/templates/TemplateHomepage.js
@@ -59,7 +59,7 @@ function TemplateHomepage() {
 			<article>
 				<Masthead title={masthead.name} logo="masthead-title-logo.webp" image={masthead.backdrop_path} summary={masthead.overview} />
 				<Listing endpoint={requests.topRated} featured header="Top 10 TV Programmes in the UK Today" class="c-listing--featured" />
-				<Listing endpoint={requests.netflixOriginals} header="Netflic Originals" />
+				<Listing endpoint={requests.netflixOriginals} header="Netflix Originals" />
 				<Listing endpoint={requests.trending} header="Currenty Trending" />
 				<Listing endpoint={requests.actionMovies} header="Action Movies" />
 				<Listing endpoint={requests.comedyMovies} header="Comedies" />
--- src/resources/js/TMDB/Requests.js
+++ src/resources/js/TMDB/Requests.js
@@ -1,11 +1,11 @@
 const requests = {
-    trending: `/trending/all/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&append_to_response=videos`,
-    netflixOriginals: `/discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_networks=213&append_to_response=videos`,
-    topRated: `/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&append_to_response=videos`,
-    actionMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=28&append_to_response=videos`,
-    comedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=35&append_to_response=videos`,
-    romanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=10749&append_to_response=videos`,
-    documentaries: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=99&append_to_response=videos`,
+    trending: `/trending/all/week?api_key=c18f3ee3879937a201e1761c73ba7940&language=en-US&append_to_response=videos`,
+    netflixOriginals: `/discover/tv?api_key=c18f3ee3879937a201e1761c73ba7940&with_networks=213&append_to_response=videos`,
+    topRated: `/movie/top_rated?api_key=c18f3ee3879937a201e1761c73ba7940&language=en-US&append_to_response=videos`,
+    actionMovies: `/discover/movie?api_key=c18f3ee3879937a201e1761c73ba7940&with_genres=28&append_to_response=videos`,
+    comedyMovies: `/discover/movie?api_key=c18f3ee3879937a201e1761c73ba7940&with_genres=35&append_to_response=videos`,
+    romanceMovies: `/discover/movie?api_key=c18f3ee3879937a201e1761c73ba7940&with_genres=10749&append_to_response=videos`,
+    documentaries: `/discover/movie?api_key=c18f3ee3879937a201e1761c73ba7940&with_genres=99&append_to_response=videos`,
 }
 
 export default requests
